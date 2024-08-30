f28f33670cac6c94991c6b5b6d60f72f26cde73f
Jay Sawant <jaysawant@Jays-MacBook-Pro.local>
2024-08-30 14:41:53 +0530

    assert syntax fixed

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ def my_sum(input_data):
     pass
 
 
-def test_zero():
-    assert(
-        my_sum("") == 0,
+def test_empty_string():
+    assert my_sum("") == 0, (
         "Output should be zero"
+    )
+
+def test_one_number():
+    assert my_sum("1") == 1, (
+        "Output should be 1"
     )
 
 
 if __name__ == "__main__":
-    test_zero()
+    test_empty_string()
+    # test_one_number()
     print("All tests passed successfully!!!")
