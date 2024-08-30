7565629a6e267987f86796a2d412d2d8fc6db9cf
Jay Sawant <jaysawant@Jays-MacBook-Pro.local>
2024-08-30 15:04:25 +0530

    test cases for multiple numubers and new line as delimiter added

--- main.py
+++ main.py
@@ -25,8 +25,26 @@ def test_one_number():
         "Output should be 1"
     )
 
+def test_two_numbers():
+    assert my_sum("1,2") == 3, (
+        "Output should be 3"
+    )
+
+def test_three_numbers():
+    assert my_sum("1,2,3") == 6, (
+        "Output should be 6"
+    )
+
+def test_with_new_line_as_delimiter():
+    assert my_sum("1\n2,3") == 6, (
+        "Output should be 6"
+    )
+
 
 if __name__ == "__main__":
     test_empty_string()
     test_one_number()
+    test_two_numbers()
+    test_three_numbers()
+    test_with_new_line_as_delimiter()
     print("All tests passed successfully!!!")
