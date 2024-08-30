fffcf3d4220dd252c538fcfe4bd0e0a9a17781d0
Jay Sawant <jaysawant@Jays-MacBook-Pro.local>
2024-08-30 14:37:24 +0530

    Test case for empty string added

---
+++ main.py
@@ -0,0 +1,15 @@
+
+def my_sum(input_data):
+    pass
+
+
+def test_zero():
+    assert(
+        my_sum("") == 0,
+        "Output should be zero"
+    )
+
+
+if __name__ == "__main__":
+    test_zero()
+    print("All tests passed successfully!!!")
